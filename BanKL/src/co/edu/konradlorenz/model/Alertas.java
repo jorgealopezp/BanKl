76647e928bfedf85881cabcf9010a55dad981c35
jorgealopezp <159073082+jorgealopezp@users.noreply.github.com>
2025-03-20 15:20:04 -0500

    Alertas 1.1

--- BanKL/src/co/edu/konradlorenz/model/Alertas.java
+++ BanKL/src/co/edu/konradlorenz/model/Alertas.java
@@ -3,7 +3,10 @@ package co.edu.konradlorenz.model;
 import java.util.List;
 
 public interface Alertas {
-    void registrarAlerta(String tipo, String descripcion);
+
+    void registrarAlerta(int id, String tipo, String descripcion);
+
     List<Alerta> revisarAlertas();
+
     void tomarAccion(int idAlerta, String accion);
 }
